9d73ab3ce5d3b5638a440a527e617bad8a9f4683
Duc Phan <phanquiduc@gmail.com>
2024-04-15 01:25:58 +0700

    Initial

---
+++ src/main/java/com/ducphan/demo/mymavenapp/MymavenappApplication.java
@@ -0,0 +1,13 @@
+package com.ducphan.demo.mymavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymavenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymavenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducphan/demo/mymavenapp/MymavenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducphan.demo.mymavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymavenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
