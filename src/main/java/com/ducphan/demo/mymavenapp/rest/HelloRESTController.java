ef0a42e3160f5007e491c9ace13067c9326d7610
Duc Phan <phanquiduc@gmail.com>
2024-04-15 18:18:11 +0700

    Initial

--- src/main/java/com/ducphan/demo/mymavenapp/rest/HelloRESTController.java
+++ src/main/java/com/ducphan/demo/mymavenapp/rest/HelloRESTController.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
+	// expose "/" that return "Hello World"
 	@GetMapping("/")
 	public String hello() {
 		return "Hello World";
+	}
+	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard workout!";
+	}
+	
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day!";
 	}
 }
