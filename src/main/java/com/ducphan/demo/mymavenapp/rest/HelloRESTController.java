6150efc55e3ba9172cffdbf4194cc2bff57dda84
Duc Phan <phanquiduc@gmail.com>
2024-04-17 18:43:49 +0700

    Initial

--- src/main/java/com/ducphan/demo/mymavenapp/rest/HelloRESTController.java
+++ src/main/java/com/ducphan/demo/mymavenapp/rest/HelloRESTController.java
@@ -1,11 +1,24 @@
 package com.ducphan.demo.mymavenapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
-
+	
+	// inject properties for: coach.name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+	@Value("${team.name}")
+	private String teamName;
+	
+	// expose "/" that return "Hello World"
+	@GetMapping("/team-info")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
+	
 	// expose "/" that return "Hello World"
 	@GetMapping("/")
 	public String hello() {
