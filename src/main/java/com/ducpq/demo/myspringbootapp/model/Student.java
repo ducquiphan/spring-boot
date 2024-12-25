5f9812eb6807501d7a3a9f5f964e9695303b9f22
Duc Phan <phanquiduc@gmail.com>
2024-12-25 13:43:22 +0700

    Initial

--- src/main/java/com/ducpq/demo/myspringbootapp/model/Student.java
+++ src/main/java/com/ducpq/demo/myspringbootapp/model/Student.java
@@ -1,7 +1,6 @@
 package com.ducpq.demo.myspringbootapp.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +20,11 @@ public class Student {
 	@Size(min = 1, max = 255, message = "Your last name length must be between 1 and 255")
 	private String lastName;
 	private Gender gender;
+	@NotNull(message = "Age cannot be empty")
+	@Min(value = 6, message = "Age must be greater than or equal to 6")
+	@Max(value = 150, message = "Age must be less than or equal to 150")
+	@Pattern(regexp = "^\\d+", message = "Only numbers are allowed")
+	private Integer age;
 	private String email;
 	private Country country;
 	private List<Integer> hobbyIds;
