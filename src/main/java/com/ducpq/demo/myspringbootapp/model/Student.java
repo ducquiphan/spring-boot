7882ac0048e3f4f4059b83fcc92b7a68eccb383a
Duc Phan <phanquiduc@gmail.com>
2024-12-25 16:12:20 +0700

    Initial

--- src/main/java/com/ducpq/demo/myspringbootapp/model/Student.java
+++ src/main/java/com/ducpq/demo/myspringbootapp/model/Student.java
@@ -23,9 +23,13 @@ public class Student {
 	@NotNull(message = "Age cannot be empty")
 	@Min(value = 6, message = "Age must be greater than or equal to 6")
 	@Max(value = 150, message = "Age must be less than or equal to 150")
-	@Pattern(regexp = "^\\d+", message = "Only numbers are allowed")
 	private Integer age;
+	@Email
 	private String email;
+	@NotNull(message = "Phone cannot be empty")
+	@Pattern(regexp = "^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4\\d|5[1|2|4-9]|6[0-3|9]|7[0-7]|8\\d|9[0-4|6|7|9])" +
+			"|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])(\\d{7})$", message = "Phone number is in incorrect format!")
+	private String phone;
 	private Country country;
 	private List<Integer> hobbyIds;
 	private String password;
