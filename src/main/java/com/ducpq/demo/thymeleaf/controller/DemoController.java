8d664d8f22c9d13a4213d051b2de9c6746b6e1ed
Duc Phan <phanquiduc@gmail.com>
2025-01-04 13:23:46 +0700

    Initial

---
+++ src/main/java/com/ducpq/demo/thymeleaf/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.ducpq.demo.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DemoController {
+	
+	@GetMapping("")
+	public String index() {
+		return "home";
+	}
+}
