684a1a4b24b692020ebf8959081f38b32011120d
Duc Phan <phanquiduc@gmail.com>
2025-01-20 14:05:34 +0700

    Initial

--- src/main/java/com/ducpq/demo/myspringbootapp/MyspringbootappApplication.java
+++ src/main/java/com/ducpq/demo/myspringbootapp/MyspringbootappApplication.java
@@ -6,14 +6,13 @@ package com.ducpq.demo.myspringbootapp;
 
 import com.ducpq.demo.myspringbootapp.game.GameRunner;
 import com.ducpq.demo.myspringbootapp.game.SupperContraGame;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyspringbootappApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootappApplication.class, args);
+		//		SpringApplication.run(MyspringbootappApplication.class, args);
 		//		MarioGame game = new MarioGame();
 		SupperContraGame game = new SupperContraGame();
 		GameRunner runner = new GameRunner(game);
